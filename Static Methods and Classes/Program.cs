eee068f8f93f66aa542a136d29fe3c5a52a1438d
oliviaalchemy <olivia@alchemytechs.com>
2019-08-08 14:50:00 +0100

    Add project files.

---
+++ Static Methods and Classes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Static_Methods_and_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            UsefulTools.SayHi("Mike");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Static Methods and Classes/UsefulTools.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Methods_and_Classes
+{
+    static class UsefulTools
+    {
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hello " + name); 
+        }
+    }
+}
